78d04d291295eb72535e3ceb582355254c619086
Ethan James <necrozma@catgirlsaresexy.org>
2024-01-20 22:17:08 -0700

    god damn this sucks

---
+++ src/app/app/components/AddRemoveLayout.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { WidthProvider, Responsive } from "react-grid-layout";
+import _ from "lodash";
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+/**
+ * This layout demonstrates how to use a grid with a dynamic number of elements.
+ */
+export default class AddRemoveLayout extends React.PureComponent {
+  static defaultProps = {
+    className: "layout",
+    cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+    rowHeight: 100
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [0, 1, 2, 3, 4].map(function(i, key, list) {
+        return {
+          i: i.toString(),
+          x: i * 2,
+          y: 0,
+          w: 2,
+          h: 2,
+          add: i === (list.length - 1)
+        };
+      }),
+      newCounter: 0
+    };
+
+    this.onAddItem = this.onAddItem.bind(this);
+    this.onBreakpointChange = this.onBreakpointChange.bind(this);
+  }
+
+  createElement(el) {
+    const removeStyle = {
+      position: "absolute",
+      right: "2px",
+      top: 0,
+      cursor: "pointer",
+    };
+    const i = el.add ? "+" : el.i;
+    return (
+      <div key={i} data-grid={el} className="grid-item bg-gray-500 flex items-center justify-center">
+        {el.add ? (
+          <span
+            className="add text text-white bg-blue-500 p-2 rounded cursor-pointer"
+            onClick={this.onAddItem}
+            title="You can add an item by clicking here, too."
+          >
+            Add +
+          </span>
+        ) : (
+          <span className="text text-white">{i}</span>
+        )}
+        <span
+          className="remove text text-white bg-red-500 p-2 rounded cursor-pointer hover:bg-red-600 mt-3 mr-3"
+          style={removeStyle}
+          onClick={this.onRemoveItem.bind(this, i)}
+        >
+          x
+        </span>
+      </div>
+    );
+  }
+
+  onAddItem() {
+    /*eslint no-console: 0*/
+    console.log("adding", "n" + this.state.newCounter);
+    this.setState({
+      // Add a new item. It must have a unique key!
+      items: this.state.items.concat({
+        i: "n" + this.state.newCounter,
+        x: (this.state.items.length * 2) % (this.state.cols || 12),
+        y: Infinity, // puts it at the bottom
+        w: 2,
+        h: 2
+      }),
+      // Increment the counter to ensure key is always unique.
+      newCounter: this.state.newCounter + 1
+    });
+  }
+
+  // We're using the cols coming back from this to calculate where to add new items.
+  onBreakpointChange(breakpoint, cols) {
+    this.setState({
+      breakpoint: breakpoint,
+      cols: cols
+    });
+  }
+
+  onLayoutChange(layout) {
+    this.props.onLayoutChange(layout);
+    this.setState({ layout: layout });
+  }
+
+  onRemoveItem(i) {
+    console.log("removing", i);
+    this.setState({ items: _.reject(this.state.items, { i: i }) });
+  }
+
+  render() {
+    return (
+      <div className="bg-gray-900">
+        <button className="bg-green-600" onClick={this.onAddItem}>Add Item</button>
+        <ResponsiveReactGridLayout
+          onLayoutChange={this.onLayoutChange}
+          onBreakpointChange={this.onBreakpointChange}
+          {...this.props}
+        >
+          {_.map(this.state.items, el => this.createElement(el))}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
+  }
+}
--- src/app/app/page.jsx
+++ src/app/app/page.jsx
@@ -1,27 +1,36 @@
 'use client';
-import React from "react";
+{/*
+import React, {useEffect} from "react";
 import _ from "lodash";
 
 import { Responsive, WidthProvider } from "react-grid-layout";
 import { Button } from "@nextui-org/react";
 import { Navbar } from "../components/nav-bar.component";
+import { doc } from "firebase/firestore";
 
 export default function Home() {
-  /*
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 }
-  ];
-  */
+  
 
   const ResponsiveGridLayout = WidthProvider(Responsive);
-  const layout = generateLayout();
+
+  let layout = generateLayout();
+
+
+
   return (
     <div>
       <Navbar />
 
-      <div id="DragEditor" className="bg-gray-200 m-10">
+      <Button
+        className="text-sm font-normal text-default-600 bg-default-100 m-2"
+        startContent="X"
+        variant="flat"
+        onClick={() => randomizeElements()}
+      >
+        Randomize
+        </Button>
+
+      <div id="DragEditor" className="bg-gray-200 m-10 overflow-auto h-[700px] border-2 border-gray-400 rounded-md">
         <ResponsiveGridLayout
         className="layout"
         layouts={layout}
@@ -32,7 +41,7 @@ export default function Home() {
       >
         {_.map(layout, function(item, i) {
           return (
-            <div key={i} className="bg-gray-500 flex items-center justify-center" id={i}>
+            <div key={i} className="bg-gray-500 flex items-center justify-center grid-item" id={i}>
               <span>{i}</span>
 
               <div className="absolute top-0 right-0">
@@ -56,20 +65,95 @@ export default function Home() {
 }
 
 function generateLayout() {
-  return _.map(_.range(0, 25), function(item, i) {
-    var y = Math.ceil(Math.random() * 4) + 1;
+  const numItems = 12; // Set the desired number of items
+  return _.map(_.range(0, numItems), function (item, i) {
     return {
       x: (i * 2) % 12,
       y: Math.floor(i / 6),
       w: 2,
-      h: y,
+      h: Math.ceil(Math.random() * 4) + 1,
       i: i.toString(),
       resizeHandles: ["n", "e", "s", "w", "ne", "se", "sw", "nw"],
     };
   });
 }
 
+
+
 function hideElement(id) {
   var element = document.getElementById(id);
   element.remove();
+}
+
+function randomizeElements() {
+  var elements = document.getElementsByClassName("grid-item");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.backgroundColor = getRandomColor();
+  }
+}
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++)
+    color += letters[Math.floor(Math.random() * 16)];
+  return color;
+}
+*/}
+
+import React, { useEffect, useState } from "react";
+import _ from "lodash";
+import { Responsive, WidthProvider } from "react-grid-layout";
+import { Button } from "@nextui-org/react";
+import { Navbar } from "../components/nav-bar.component";
+import { doc } from "firebase/firestore";
+import AddRemoveLayout from "./components/AddRemoveLayout"; // Import the AddRemoveLayout component
+
+export default function Home() {
+  const ResponsiveGridLayout = WidthProvider(Responsive);
+  const [layout, setLayout] = useState(generateLayout());
+
+  const onLayoutChange = newLayout => {
+    setLayout(newLayout);
+  };
+
+  function generateLayout() {
+    const numItems = 12; // Set the desired number of items
+    return _.map(_.range(0, numItems), function (item, i) {
+      return {
+        x: (i * 2) % 12,
+        y: Math.floor(i / 6),
+        w: 2,
+        h: Math.ceil(Math.random() * 4) + 1,
+        i: i.toString(),
+        resizeHandles: ["n", "e", "s", "w", "ne", "se", "sw", "nw"],
+      };
+    });
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <Button
+        className="text-sm font-normal text-default-600 bg-default-100 m-2"
+        startContent="X"
+        variant="flat"
+        onClick={() => randomizeElements()}
+      >
+        Randomize
+      </Button>
+
+      <div id="DragEditor" className="m-10">
+        <AddRemoveLayout
+          className="layout"
+          layouts={layout}
+          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+          resizeHandles={["n", "e", "s", "w", "ne", "se", "sw", "nw"]}
+          compactType={null}
+          onLayoutChange={onLayoutChange}
+        />
+      </div>
+    </div>
+  );
 }
